98f813f7378ec330492b05115f7584dacc85a4bb
Stevan Bogosavljevic <stevyhacker@gmail.com>
2022-07-13 09:50:28 -0500

    Add Moonriver chain and testnet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,56 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import {RainbowKitProvider, getDefaultWallets, Chain} from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
+const moonRiverChain: Chain = {
+    id: 1285,
+    name: 'Moonriver',
+    network: 'moonriver',
+    iconUrl: 'https://moonriver.moonscan.io/images/svg/brands/main.svg?v=22.7.1.0',
+    iconBackground: '#fff',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'Moonriver',
+        symbol: 'MOVR',
+    },
+    rpcUrls: {
+        default: 'https://rpc.api.moonriver.moonbeam.network',
+    },
+    blockExplorers: {
+        default: { name: 'MoonScan', url: 'https://moonriver.moonscan.io/' },
+    },
+    testnet: false,
+};
+
+const moonBaseChain: Chain = {
+    id: 1287,
+    name: 'Moonbase Alpha',
+    network: 'moonbase',
+    iconUrl: 'https://moonbase.moonscan.io/images/svg/brands/main.svg?v=22.7.1.0',
+    iconBackground: '#fff',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'Dev token',
+        symbol: 'DEV',
+    },
+    rpcUrls: {
+        default: 'https://rpc.api.moonbase.moonbeam.network',
+    },
+    blockExplorers: {
+        default: { name: 'MoonScan', url: 'https://moonbase.moonscan.io/' },
+    },
+    testnet: true,
+};
+
 const { chains, provider, webSocketProvider } = configureChains(
   [
-    chain.mainnet,
-    chain.polygon,
-    chain.optimism,
-    chain.arbitrum,
+    moonRiverChain,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
-      ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
+      ? [moonBaseChain]
       : []),
   ],
   [
