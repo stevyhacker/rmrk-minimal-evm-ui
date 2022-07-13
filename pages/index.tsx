8079d859b6d434dab53c59f1cbcdc9a5c15cbdde
Stevan Bogosavljevic <stevyhacker@gmail.com>
2022-07-13 11:26:19 -0500

    Clean up page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+import type { NextPage } from "next"
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
   return (
@@ -19,66 +19,38 @@ const Home: NextPage = () => {
         <ConnectButton />
 
         <h1 className={styles.title}>
-          Welcome to <a href="">RainbowKit</a> + <a href="">wagmi</a> +{' '}
-          <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://rmrk.app">RMRK!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        <p className={styles.description}>To get started try one of these and mint an NFT:</p>
+        <h2 className={styles.description}>
+          <a href="/nesting">Nesting </a>
+          or
+          <a href="/multi-resource"> Multi-resource</a>
+        </h2>
 
         <div className={styles.grid}>
-          <a href="https://rainbowkit.com" className={styles.card}>
-            <h2>RainbowKit Documentation &rarr;</h2>
-            <p>Learn how to customize your wallet connection flow.</p>
+          <a href="/nesting" className={styles.card}>
+            <h2>Nesting Demo &rarr;</h2>
+            <p>Make your NFT own other NFTs.</p>
           </a>
 
-          <a href="https://wagmi.sh" className={styles.card}>
-            <h2>wagmi Documentation &rarr;</h2>
-            <p>Learn how to interact with Ethereum.</p>
+          <a href="/multi-resource" className={styles.card}>
+            <h2>Multi-resource Demo &rarr;</h2>
+            <p>Multiple file types inside one NFT.</p>
           </a>
 
-          <a
-            href="https://github.com/rainbow-me/rainbowkit/tree/main/examples"
-            className={styles.card}
-          >
-            <h2>RainbowKit Examples &rarr;</h2>
-            <p>Discover boilerplate example RainbowKit projects.</p>
+          <a href="/equippable" className={styles.card}>
+            <h2>Equippable NFTs Demo &rarr;</h2>
+            <p>Equip items to your NFT characters.</p>
           </a>
 
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Next.js Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Next.js Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a href="https://rainbow.me" target="_blank" rel="noopener noreferrer">
-          Made with ❤️ by your frens at 🌈
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
