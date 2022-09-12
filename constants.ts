dd4f99ce1a1a8a5bd30755f9c8f69a14d2eb74a3
Stevan Bogosavljevic <stevyhacker@gmail.com>
2022-09-12 14:54:54 +0200

    Abstract contract addresses into an env file

--- constants.ts
+++ constants.ts
@@ -1,20 +1,12 @@
 import abis from './abis/abis';
 
-// MoonbaseAlpha deployments
-// export const RMRKMultiResourceFactoryContractAddress = '0x8E1214B2eD8a8581D1f35f525d82E9077C2c7513';
-// export const RMRKNestingFactoryContractAddress = '0x2a962fA5DD393507B2b1C527d2aa52EBC298f689';
-// export const RMRKMarketplaceContractAddress = '0xbdf6DB34C85669DCf090781d43b8F5391DE38DEd';
-
-// Goerli deployments
-export const RMRKMultiResourceFactoryContractAddress = '0x35599CfFCdC8Bb29e77e316Be974D31B6a932eD2';
-export const RMRKNestingFactoryContractAddress = '0xa8660beBD6F526bd6614983E98eB95E7905C822A';
-export const RMRKEquippableFactoryContractAddress = '0xAC655Fd14a175fDE5B2e66e66Ba0Ae67164025e7';
-export const RMRKMarketplaceContractAddress = '0x3151f12E6A8e1941278b636b2E3850d1eFE37A7E';
+export const RMRKMultiResourceFactoryContractAddress = process.env.NEXT_PUBLIC_RMRK_MULTI_RESOURCE_FACTORY_CONTRACT_ADDRESS;
+export const RMRKNestingFactoryContractAddress = process.env.NEXT_PUBLIC_RMRK_NESTING_FACTORY_CONTRACT_ADDRESS;
+export const RMRKEquippableFactoryContractAddress = process.env.NEXT_PUBLIC_RMRK_EQUIPPABLE_FACTORY_CONTRACT_ADDRESS;
+export const RMRKMarketplaceContractAddress = process.env.NEXT_PUBLIC_RMRK_MARKETPLACE_CONTRACT_ADDRESS;
 
 export const NATIVE_ETH = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
 
-//Moonscan MR https://moonbase.moonscan.io/address/0xB77520a5D97CAF7DEBC5AF7A28C19c49F063B4A5
-//Moonscan Nesting https://moonbase.moonscan.io/address/0xd0c004801Af669857Bf0241235b7cBC3897F5932
 
 export const multiResourceFactoryContractDetails = {
   addressOrName: RMRKMultiResourceFactoryContractAddress,
